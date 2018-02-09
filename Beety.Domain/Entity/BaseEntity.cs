01f2ec2f9600683bdb92cca8cd37008dd6aa5425
Zubel, Artiom <artiom0724@gmail.com>
2018-02-10 02:38:43 +0300

    task/BEETY#3 - Implement users Entity(Add entities, but DB not working)

---
+++ Beety.Domain/Entity/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Beety.Domain.Entity
+{
+    public class BaseEntity
+    {
+        [Key]
+        public long Id { get; set; }
+    }
+}
