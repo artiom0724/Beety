875433f0ce09160619db0217d1937345c25a6854
Sergey Pastushenko <s.pastushenko@itransition.com>
2018-02-10 23:12:31 +0300

    added basic back-ned

---
+++ Models/Security/Role.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models.Security
+{
+    public class Role: EntityBase
+    {
+        [StringLength(255)]
+        public string RoleName { get; set; }
+
+        public virtual IList<User> Users { get; set; }
+    }
+}
---
+++ Models/Security/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Models.Security
+{
+    public class User: EntityBase
+    {
+        [StringLength(30)]
+        public string UserName { get; set; }
+
+        [StringLength(30)]
+        public string FirstName { get; set; }
+
+        [StringLength(40)]
+        public string LastName { get; set; }
+
+        public string PasswordSalt { get; set; }
+
+        public string Password { get; set; }
+
+        [Required]
+        public virtual RegistrationToken Token { get; set; }
+
+        public DateTime? RegisteredAt { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedAt { get; set; }
+
+        public int FailedLoginAttemptsCount { get; set; }
+
+        [StringLength(255)]
+        public string EmailAddress { get; set; }
+    }
+}
